a8b71011850fc9a110506d0483120937c09afaa9
Rob Dongas <dongasr@gmail.com>
2023-05-01 15:07:13 +1000

    Possible solutions for using localStorage

--- public/script.js
+++ public/script.js
@@ -2,11 +2,10 @@
 const form = document.getElementById("taskform");
 const tasklist = document.getElementById("tasklist");
 
+
+// Handle form submission, using input values to add new task
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-
-    console.log(form.elements.taskType.value)
-
     addTask(
         form.elements.taskName.value,
         form.elements.taskType.value,
@@ -14,68 +13,62 @@ form.addEventListener("submit", function (event) {
         form.elements.taskTime.value,
         form.elements.taskClient.value,
     )
-    console.log(taskList)
 })
 
-function displayTask(task) {
-    let item = document.createElement("li");
-    item.setAttribute("data-id", task.id);
-    item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
 
-    tasklist.appendChild(item);
+// General function for fetching tasks from localStorage and rendering to screen
+function displayTasks() {
 
-    // Clear the value of the input once the task has been added to the page
-    form.reset();
+    // Clear the tasklist <ul> element's content
+    tasklist.innerHTML = ""
 
-    // Setup delete button DOM elements
-    let delButton = document.createElement("button");
-    let delButtonText = document.createTextNode("Delete");
-    delButton.appendChild(delButtonText);
-    item.appendChild(delButton); // Adds a delete button to every task
+    // Fetch and parse tasks array from localStorage
+    let localTasks = JSON.parse(localStorage.getItem('tasks'))
 
-    // Listen for when the delete button is clicked
-    delButton.addEventListener("click", function (event) {
+    // If there are tasks (localStorage item exists)
+    if (localTasks !== null) {
 
-        taskList.forEach(function (taskArrayElement, taskArrayIndex) {
-            if (taskArrayElement.id == item.getAttribute('data-id')) {
-                taskList.splice(taskArrayIndex, 1)
-            }
+        // Loop through all tasks in the array
+        localTasks.forEach(function(task) {
+
+            // Create new list item and populate with content (including data attribute for ID)
+            let item = document.createElement("li");
+            item.setAttribute("data-id", task.id);
+            item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
+            tasklist.appendChild(item);
+
+            // Clear the value of the input once the task has been added to the page
+            form.reset();
+
+            // Setup delete button DOM elements
+            let delButton = document.createElement("button");
+            let delButtonText = document.createTextNode("Delete");
+            delButton.appendChild(delButtonText);
+            item.appendChild(delButton); // Adds a delete button to every task
+
+            // Listen for when the delete button is clicked
+            delButton.addEventListener("click", function (event) {
+
+                // Loop through all the tasks to find the matching ID and remove it from the array
+                localTasks.forEach(function(taskArrayElement, taskArrayIndex) {
+                    if (taskArrayElement.id == item.getAttribute('data-id')) {
+                        localTasks.splice(taskArrayIndex, 1)
+                    }
+                })
+
+                // Update localStorage with the newly spliced array (converted to a JSON string)
+                localStorage.setItem('tasks', JSON.stringify(localTasks))
+
+                item.remove(); // Remove the task item from the page when button clicked
+                // Because we used 'let' to define the item, this will always delete the right element
+
+            })
         })
 
-        // Make sure the deletion worked by logging out the whole array
-        console.log(taskList)
-
-        item.remove(); // Remove the task item from the page when button clicked
-        // Because we used 'let' to define the item, this will always delete the right element
-
-    })
-
+    }
 
 }
 
-
-
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
-var taskList = [];
 
 // Create a function called 'addTask'
 // Give the function input parameters for: name, type, rate, time, client
@@ -84,18 +77,6 @@ var taskList = [];
 // Add the object to the taskList array
 
 function addTask(name, type, rate, time, client) {
-
-    // Creating the object with the usual property:value syntax
-    // Create task object 
-    // let task = {
-    //   name: name,
-    //   type: type,
-    //   id: Date.now(),
-    //   date: new Date().toISOString(),
-    //   rate: rate,
-    //   time: time,
-    //   client: client
-    // }
 
     // Creating the object, directly passing in the input parameters
     let task = {
@@ -108,13 +89,29 @@ function addTask(name, type, rate, time, client) {
         client
     }
 
-    taskList.push(task);
-    displayTask(task);
+    // Fetch and parse tasks array from localStorage 
+    let localTasks = JSON.parse(localStorage.getItem('tasks'))
+
+    // If no tasks exist in local storage, create a new array using the current task
+    if (localTasks == null) {
+        localTasks = [task]
+    } else {
+        // Otherwise check to see if a task with the same ID already exists (just in case)
+        if (localTasks.find(element => element.id === task.id)) {
+            console.log('Task ID already exists')
+        } else {
+            // If not, push the new task to the array
+            localTasks.push(task);
+        }
+    }
+
+    // Update localStorage with the array (converted to a JSON string)
+    localStorage.setItem('tasks', JSON.stringify(localTasks))
+
+    // Call function to display the tasks on the DOM
+    displayTasks();
 
 }
 
 // Call the function with test values for the input paramaters
 addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
-
-// Log the array to the console.
-console.log(taskList);
